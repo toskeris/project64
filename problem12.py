772f2849edf4cf9c614f23fd55b8e48ded80dd55
Torgeir Skovholt <torgeir.skovholt@outlook.com>
2020-06-22 13:04:56 +0200

    la til problem13.py

--- problem12.py
+++ problem12.py
@@ -19,18 +19,50 @@ What is the value of the first triangle number to have over five hundred divisor
 
 """
 
-def getfactors(n):
-    arr = []
-    for i in range(0, int(n)):
-        if(n % (i + 1) == 0):
-            arr.append(i + 1)
-    return arr
-
-def trianglenumber(n):
-    return n * (n + 1) / 2
+import time
+from math import sqrt
 
 
-i = 10
-while len(getfactors(trianglenumber(i))) < 501:
-    i+=1
-print(trianglenumber(i))
+def count_divisors(n):
+    d = {}
+    count = 1
+
+    while n % 2 == 0:
+        n = n / 2
+        try:
+            d[2] += 1
+        except KeyError:
+            d[2] = 1
+
+    for i in range(3, int(sqrt(n+1)), 2):
+        while n % i == 0 and i != n:
+            n = n / i
+            try:
+                d[i] += 1
+            except KeyError:
+                d[i] = 1
+
+    d[n] = 1
+
+    for _,v in d.items():
+        count = count * (v + 1)
+
+    return count
+
+def tri_number(num):
+  next = 1 + int(sqrt(1+(8 * num)))
+  return num + (next/2)
+
+
+def main():
+    i = 1
+    while count_divisors(i) < 500:
+        i = tri_number(i)
+    return i
+
+
+start = time.time()
+answer = main()
+elapsed = (time.time() - start)
+
+print("result %s returned in %s seconds." % (answer, elapsed))
